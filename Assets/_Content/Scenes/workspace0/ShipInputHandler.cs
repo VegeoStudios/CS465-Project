d1572ccbe7eaf22a5831857a8de6a372b1236be2
VegeoStudios <vegeo.studios@gmail.com>
2025-03-16 21:54:50 -0600

    Checkpoint 1

--- Assets/_Content/Scenes/workspace0/ShipInputHandler.cs
+++ Assets/_Content/Scenes/workspace0/ShipInputHandler.cs
@@ -2,11 +2,11 @@ using UnityEngine;
 
 public class ShipInputHandler : MonoBehaviour
 {
-    public float Throttle { get; private set; }
-    public float Pitch { get; private set; }
-    public float Roll { get; private set; }
-    public float Yaw { get; private set; }
-    public float VerticalThrust { get; private set; }
-    public float HorizontalThrust { get; private set; }
-    public bool Boost { get; private set; }
+    public float Throttle;
+    public float Pitch;
+    public float Roll;
+    public float Yaw;
+    public float VerticalThrust;
+    public float HorizontalThrust;
+    public bool Boost;
 }
